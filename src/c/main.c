80a6478e7efe2442d11f2087cb4fd773e6bf4d7e
jonathlt <jonathan.lakethomas@gmail.com>
2017-10-08 22:06:09 +0100

    Add city

--- src/c/main.c
+++ src/c/main.c
@@ -8,7 +8,9 @@
 
 static Window *window;	
 static TextLayer *random_text_layer;
+static TextLayer *city_text_layer;
 static AppTimer *random_timer;
+static AppTimer *city_timer;
 	
 // Called when a message is received from the JavaScript side
 static void in_received_handler(DictionaryIterator *received, void *context) {
@@ -20,6 +22,12 @@ static void in_received_handler(DictionaryIterator *received, void *context) {
 	} else {
         text_layer_set_text(random_text_layer, "ERROR!");
     }
+  tuple = dict_find(received, MESSAGE_KEY_CITY);
+  if (tuple) {
+        text_layer_set_text(city_text_layer, tuple->value->cstring);
+  } else {
+        text_layer_set_text(city_text_layer, "ERROR!");
+  }
 }
 
 // Make a request for the random number
@@ -33,6 +41,20 @@ static void ask_for_random(void *context) {
         app_message_outbox_send();
     }
     random_timer = app_timer_register(5000, ask_for_random, NULL);
+}
+
+// Make a request for the city
+static void ask_for_city(void *context) {
+   DictionaryIterator *iter;
+  
+   uint32_t result = app_message_outbox_begin(&iter);
+  
+   if (result == APP_MSG_OK) {
+     dict_write_int8(iter, MESSAGE_KEY_CITY, 1);
+     dict_write_end(iter);
+     app_message_outbox_send();
+   }
+   city_timer = app_timer_register(10000, ask_for_city, NULL);
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -63,23 +85,32 @@ static void init(void) {
     window_stack_push(window, animated);
 
     // Register AppMessage handlers
-	app_message_register_inbox_received(in_received_handler); 
+	  app_message_register_inbox_received(in_received_handler); 
 
     // Initialize AppMessage inbox and outbox buffers with a suitable size
     const int inbox_size = 128;
     const int outbox_size = 128;
     app_message_open(inbox_size, outbox_size);
 
-    // Configure the debt text layer
+    // Configure the random text layer
     random_text_layer = text_layer_create(GRect(10,60, frame.size.w-20, frame.size.h-120));
     text_layer_set_font(random_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_background_color(random_text_layer, GColorWhite);
     text_layer_set_text_color(random_text_layer, GColorRed);
     text_layer_set_text_alignment(random_text_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(random_text_layer));
+  
+    // Configure the city text layer
+    city_text_layer = text_layer_create(GRect(10, 80, frame.size.w-20, frame.size.h-140));
+    text_layer_set_font(city_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+    text_layer_set_background_color(city_text_layer, GColorWhite);
+    text_layer_set_text_color(city_text_layer, GColorRed);
+    text_layer_set_text_alignment(city_text_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(city_text_layer));
     
-    // Set the timer
+    // Set the timers
     random_timer = app_timer_register(5000, ask_for_random, NULL);
+    city_timer = app_timer_register(10000, ask_for_city, NULL);
 }
 
 static void deinit(void) {
--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -6,6 +6,9 @@
 //  Mike Jipping, August, 2016
 
 var randomURL = "https://www.random.org/integers/?num=1&min=1&max=50&col=1&base=10&format=plain&rnd=new";
+var myAPIKey = 'fd541b64f6da378322b01f9a122d5dee';
+var APIURL = 'http://api.openweathermap.org/data/2.5/weather';
+var watchID;
 
 // Called when the message send attempt succeeds
 function messageSuccessHandler() {
@@ -29,6 +32,43 @@ function get_random() {
   return page;
 }
 
+function getLocation() {
+  watchID = navigator.geolocation.getCurrentPosition(getPlaceName, error, options);
+}
+
+ var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function getPlaceName(pos) {
+  
+  // Construct URL
+  var url = APIURL + '?' + 'lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&cnt=1&appid=' + myAPIKey;
+  console.log("URL=" + url);
+  // Send request
+  xhrRequest(url, 'GET',
+    function(responseText) {
+      // responseText contains a JSON object with city info
+      var json = JSON.parse(responseText);
+      console.log("Response text = " + responseText);
+      var city = json.name;
+      console.log("CITY retrieved = " + city);
+      Pebble.sendAppMessage({"CITY": city}, messageSuccessHandler, messageFailureHandler);
+    });
+}
+  
+function error() {  
+}
+
+function options() {
+  
+}
+
 // Called when JavaScript is ready
 Pebble.addEventListener("ready", function(e) {
   console.log("JS is ready!");
@@ -45,4 +85,8 @@ Pebble.addEventListener("appmessage", function(e) {
         console.log("Number = " + rand);
         Pebble.sendAppMessage({"RANDOM": rand}, messageSuccessHandler, messageFailureHandler);
   }
+  if (dictionary['CITY']) {
+        getLocation();
+  }
+  
 });
